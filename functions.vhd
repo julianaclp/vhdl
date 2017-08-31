a299c6082d37e525f40ad03dfac39bb4ce00fc4d
julianaclp <julianaclp350@gmail.com>
2017-08-31 12:48:30 -0300

    Correção do cálculo da média

--- functions.vhd
+++ functions.vhd
@@ -4,21 +4,20 @@ USE ieee.numeric_std.all;
 
 PACKAGE functions IS
 	PROCEDURE stats(x1, x2, x3, x4, x5, x6, x7, x8: IN SIGNED(7 DOWNTO 0); 
-		SIGNAL ave_sum, ave_div, max, min: OUT SIGNED(7 DOWNTO 0));
+		SIGNAL ave_div, max, min: OUT SIGNED(7 DOWNTO 0); 
+		SIGNAL ave_sum: OUT SIGNED(10 DOWNTO 0));
 		type order_array is array (0 to 7) of SIGNED(7 DOWNTO 0);
 END functions;
 
 PACKAGE BODY functions IS
 	PROCEDURE stats (x1, x2, x3, x4, x5, x6, x7, x8: IN SIGNED(7 DOWNTO 0); 
-		SIGNAL ave_sum, ave_div, max, min: OUT SIGNED(7 DOWNTO 0)) IS
+		SIGNAL ave_div, max, min: OUT SIGNED(7 DOWNTO 0); 
+		SIGNAL ave_sum: OUT SIGNED(10 DOWNTO 0)) IS
 	variable x : order_array;
 	variable max_temp, min_temp: SIGNED(7 DOWNTO 0);
-	variable ave_sum_temp: SIGNED(7 DOWNTO 0) := (OTHERS => '0');
+	variable ave_sum_temp: SIGNED(10 DOWNTO 0) := (OTHERS => '0');
 	BEGIN 
-		x := (x1, x2, x3, x4, x5, x6, x7, x8);
-		ave_sum_temp := x1+x2+x3+x4+x5+x6+x7+x8;
-		ave_sum <= ave_sum_temp;
-		ave_div <= ave_sum_temp/8;
+		x := (x1, x2, x3, x4, x5, x6, x7, x8);	
 		max_temp := x1;
 		min_temp := x1;
 		FOR i IN 0 TO 7 LOOP
@@ -28,7 +27,10 @@ PACKAGE BODY functions IS
 			IF x(i) < min_temp THEN 
 				min_temp := x(i);
 			END IF;
+		ave_sum_temp := ave_sum_temp + resize(x(i), ave_sum_temp'length);
 		END LOOP;
+		ave_sum <= ave_sum_temp;
+		ave_div <= resize(ave_sum_temp/8, 8);
 		max <= max_temp;
 		min <= min_temp;
 	END stats;
